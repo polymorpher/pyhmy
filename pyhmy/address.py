e6dbf873ca63ec52e6194c0acffecc2e1b99b69c
Aaron Li <aaron@qokka.ai>
2021-10-14 19:04:11 -0700

    underscore

--- pyhmy/address.py
+++ pyhmy/address.py
@@ -3,21 +3,21 @@ import binascii
 import eth_utils
 
 
-def toBHex(addr, hrp='one'):
+def to_bhex(addr, hrp='one'):
     head, data = bech32.bech32_decode(addr)
     if head != hrp:
         return None
     return bytearray(bech32.convertbits(data[:], 5, 8, False))
 
 
-def toHex(addr, hrp='one'):
-    b = toBHex(addr, hrp)
+def to_hex(addr, hrp='one'):
+    b = to_bhex(addr, hrp)
     h = binascii.hexlify(b)
     return '0x' + h.decode('utf-8')
 
 
-def toChecksum(addr, hrp='one'):
-    b = toBHex(addr)
+def to_checksum(addr, hrp='one'):
+    b = to_bhex(addr, hrp)
     if not b:
         return None
     return eth_utils.to_checksum_address(b)
